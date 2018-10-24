c117ecd62f8fefb38e7d8eb8ec4295b48b8320f0
narendrakpatel <narpatelk@gmail.com>
2018-10-24 22:47:42 +0530

    Search functionality implemented

--- src/CardList.js
+++ src/CardList.js
@@ -2,12 +2,20 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-    const CardComponent = robots.map(user => {
-        return <Card id={user.id} name={user.name} email={user.email} />
-    })
     return(
         <div>
-            {CardComponent}
+            {
+                robots.map(user => {
+                    return (
+                        <Card
+                            key={user.id}
+                            id={user.id}
+                            name={user.name}
+                            email={user.email}
+                            />
+                    );
+                })
+            }
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { robots } from './robots';
 
-ReactDOM.render(<CardList robots={robots}/>, document.getElementById('root'));
+ReactDOM.render(
+    <App />,
+    document.getElementById('root'));
 
 serviceWorker.unregister();
